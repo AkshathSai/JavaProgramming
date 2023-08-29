a610e1288ffa2686d0c9f88d0f6785362ff4f344
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2023-08-29 10:44:37 +0530

    Added Files.walk() method to search the entire directory, with maxDepth function param we can even search for the files & folders underneath

--- src/main/java/com/example/dev/files/ScanCurrentDirectory.java
+++ src/main/java/com/example/dev/files/ScanCurrentDirectory.java
@@ -2,6 +2,7 @@ package com.example.dev.files;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ScanCurrentDirectory {
@@ -9,13 +10,14 @@ public class ScanCurrentDirectory {
     public static void main(String[] args) throws IOException {
 
         /**
-         * "*" here means current directory
+         * "." here refers to current directory
          */
+        Path currentDirectory = Paths.get(".");
 
         //List files & folders in the current directory with Files.list
-        Files.list(Paths.get(".")).forEach(System.out::println);
+        Files.list(currentDirectory).forEach(System.out::println);
 
-
-
+        //Searches the entire directory including the files & folders underneath them
+        Files.walk(currentDirectory, 3).forEach(System.out::println);
     }
 }
