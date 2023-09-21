d13ab54b3f013ce0822b61a90e201d716e1c4ad1
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2023-09-21 12:53:38 +0530

    Programmed another approach to read Files in Java

--- src/main/java/com/example/dev/files/ReadFile.java
+++ src/main/java/com/example/dev/files/ReadFile.java
@@ -4,12 +4,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class ReadFile {
 
     public static void main(String[] args) throws IOException {
         // "." here refers to current directory
         Path filePath = Paths.get("./src/main/resources/data.txt");
+
+        // Before streams all the file content will be loaded into memory at once instead of a buffer like in streams
+        // List<String> lines = Files.readAllLines(filePath);
+        // System.out.println(lines);
 
         Files.lines(filePath)
                 .map(String::toLowerCase)
